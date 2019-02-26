fa9039e45842f341026480a6c60c1346a35d72e6
paul.gazel-anthoine <paul.gazel-anthoine@est.fib.upc.edu>
2019-02-26 17:22:59 +0100

    commit start dema

---
+++ src/Start.java
@@ -0,0 +1,8 @@
+public class Start {
+// COMENTARIO
+    public static void main(String[] args) {
+        Test x = new Test();
+        System.out.println("Start dema, mi número favorito es el " + x.getX());
+    }
+
+}
--- src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-// COMENTARIO
-    public static void main(String[] args) {
-        Test x = new Test();
-        System.out.println("Jordi Pringao, mi número favorito es el " + x.getX());
-    }
-
-}
