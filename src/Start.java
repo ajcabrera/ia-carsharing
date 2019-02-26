7e479da0537335a3838e5e87173e91f578fba663
ajcabrera <44556974+ajcabrera@users.noreply.github.com>
2019-02-26 17:26:50 +0100

    Masterpiece

--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,7 @@ public class Start {
 // COMENTARIO
     public static void main(String[] args) {
         Test x = new Test();
-        System.out.println("Start dema, mi número favorito es el " + x.getX());
+        System.out.println("Antonio va a sacar un 1" + x.getX());
     }
 
 }
