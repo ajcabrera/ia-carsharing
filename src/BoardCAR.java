e797ef9e96ff454be9b0647d6f0426aac0b71c44
paul.gazel-anthoine <paul.gazel-anthoine@est.fib.upc.edu>
2019-03-15 22:05:36 +0100

    Add RandomInit

--- src/BoardCAR.java
+++ src/BoardCAR.java
@@ -6,13 +6,33 @@ import java.util.Vector;
 
 public class BoardCAR {
 
-    private Vector<ArrayList<Usuario>> caminos;
+    private Vector<ArrayList<Usuario>> caminos = new Vector<ArrayList<Usuario>>;
 
 
     public BoardCAR(Vector<Usuario> pasajeros, int ncond) {
-        caminos = new Vector<>(ncond);
         // Crear solución inicial
+        RandomInit(pasajeros,ncond);
+        printBoard();
+    }
 
+    private void RandomInit(Vector<Usuario> pasajeros, int ncond) {
+        caminos = new Vector<ArrayList<Usuario>> (ncond);
+        Random rand;
+        for (Usuario i: U) {
+            int cond = rand.nextInt(ncond);
+            caminos.get(cond).add(i);
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("Board:");
+        for (ArrayList l: caminos) {
+            System.out.println("Conductor:");
+            for (int i = 0; i < l.size(); i++) {
+                Usuario aux = l.get(i);
+                System.out.println("[" + aux.getCoordOrigenX() + "," aux.getCoordOrigenY() + "]");
+            }
+        }
     }
 
 }
